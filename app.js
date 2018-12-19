2665cbaba31444f6a8b9a1a4a0b6c935641b5482
Daniel <danielborg291@gmail.com>
2018-12-18 20:31:01 -0800

    fixed error on line 26

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ $.ajax({
     dataType: 'json',
     headers: {
         'Client-ID': your_client_id
-    }
+    },
     success: function (channel) {
         if (channel["stream"] == null) {
             alert(nickname + " is not online");
