6291516e2c438958a4fb1126e303b23ec3a5177c
Daniel <danielborg291@gmail.com>
2019-07-28 08:42:32 -0700

    correct bug when allowing multiple badges to use a command

--- src/commands/Commands.ts
+++ src/commands/Commands.ts
@@ -8,14 +8,14 @@ const HelloCommand: ICommand = {
 	key: "!hello",
 	action: (client: Client, config: IConfiguration) => {
 		client.action(config.channel, "Hello!");
-	}
+	},
 };
 
 const DiscordCommand: ICommand = {
 	key: "!discord",
 	action: (client: Client, config: IConfiguration) => {
 		client.action(config.channel, "If you want to join my Discord, here is the link! " + config.discordLink);
-	}
+	},
 }
 
 const LootsCommand: ICommand = {
@@ -25,7 +25,7 @@ const LootsCommand: ICommand = {
 			config.channel,
 			"Loots is way to send a donation like request that it completely free. After a short ad is shown on the top right, your message will apear. This is way to make sure I see your message as well supporting the stream. To send a messgae go to: https://loots.com/theborglive"
 		);
-	}
+	},
 }
 
 const CommandsCommand: ICommand = {
@@ -35,7 +35,7 @@ const CommandsCommand: ICommand = {
 			config.channel,
 			"!follow    !twitchprime    !discord    !loot    !uptime    !permit  !so    !poll    !closepoll    !answer    !leave    NOTE:  Some commands can only be used a mod or the streamer."
 		);
-	}
+	},
 }
 
 const FollowCommand: ICommand = {
@@ -45,7 +45,8 @@ const FollowCommand: ICommand = {
 			config.channel,
 			"Please be sure to follow the stream so you can be notified when I go live next! It really helps the channel grow and build a great community."
 		);
-	}
+	},
+	badges: [Badge.broadcaster, Badge.moderator]
 }
 
 const TwitchPrimeCommand: ICommand = {
@@ -55,7 +56,8 @@ const TwitchPrimeCommand: ICommand = {
 			config.channel,
 			"If you wish to give back to the stream and you have Amazon Prime, you can get one free Twitch Prime subscription a month. You can subscribe by clicking above the stream or by clicking https://www.twitch.tv/products/theborgLIVE/ticket/new"
 		);
-	}
+	},
+	badges: [Badge.broadcaster, Badge.moderator]
 }
 
 const Commands: ICommand[] = [HelloCommand, DiscordCommand, LootsCommand, CommandsCommand, FollowCommand, TwitchPrimeCommand];
